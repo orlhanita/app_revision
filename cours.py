5a49d1c8b68bb0efe92d7e8366069733812e6451
Orlhanita <orlhanita@MacBook-Air-de-Orlhanita.local>
2026-03-16 12:02:56 +0100

    ajout cours

--- cours.py
+++ cours.py
@@ -1,17 +1,13 @@
 import streamlit as st
+import os
 def lire_cours(fichier):
     with open(f"cours/{fichier}", "r") as f:
         contenu = f.read()
     st.markdown(contenu)
 def afficher_cours():
-  theme = st.selectbox(
-        "Choisis un thème",
-        [
-            "Analyse fonctionnelle",
-            "SQL"
-        ]
-    )
-  if theme == "Analyse fonctionnelle":
-        lire_cours("analyse_fonctionnelle.md")
-  elif theme == "SQL":
-        lire_cours("sql.md")
+    fichiers = [f for f in os.listdir("cours") if f.endswith(".md")]
+    noms = [f.replace(".md", "").replace("_", " ").title() for f in fichiers]
+    choix = st.selectbox("Choisis un cours", noms)
+    index = noms.index(choix)
+    fichier = fichiers[index]
+    lire_cours(fichier)
