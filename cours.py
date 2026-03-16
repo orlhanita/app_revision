6271021dfc2dc9d31331b8feb22aed9e4c349f4a
Orlhanita <orlhanita@MacBook-Air-de-Orlhanita.local>
2026-03-16 12:00:24 +0100

    cours markdown

--- cours.py
+++ cours.py
@@ -1,15 +1,17 @@
 import streamlit as st
-from contenu_cours import COURS
+def lire_cours(fichier):
+    with open(f"cours/{fichier}", "r") as f:
+        contenu = f.read()
+    st.markdown(contenu)
 def afficher_cours():
-    st.header("Cours")
-    categorie = st.radio("Choisis une partie", list(COURS.keys()))
-    themes = COURS[categorie]
-    theme = st.selectbox("Choisis un thème", list(themes.keys()))
-    contenu = themes[theme]
-    if isinstance(contenu, dict):
-        sous_theme = st.selectbox("Choisis un sous-thème", list(contenu.keys()))
-        st.subheader(sous_theme)
-        st.write(contenu[sous_theme])
-    else:
-        st.subheader(theme)
-        st.write(contenu)
+  theme = st.selectbox(
+        "Choisis un thème",
+        [
+            "Analyse fonctionnelle",
+            "SQL"
+        ]
+    )
+  if theme == "Analyse fonctionnelle":
+        lire_cours("analyse_fonctionnelle.md")
+  elif theme == "SQL":
+        lire_cours("sql.md")
---
+++ cours/analyse_fonctionnelle.md
@@ -0,0 +1,11 @@
+# Analyse fonctionnelle
+## Définition
+L'analyse fonctionnelle vise à identifier les besoins des utilisateurs.
+## Objectifs
+- comprendre le besoin métier
+- définir les fonctions du système
+- rédiger le cahier des charges
+## Livrables
+- diagrammes
+- documentation
+- spécifications
